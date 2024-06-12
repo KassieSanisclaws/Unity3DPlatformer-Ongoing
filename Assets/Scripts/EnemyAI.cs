cfe8d40fd457c7c374688a88bb4a1870594cdfb2
Kadeem Cherman (Kassie) <86675041+KassieSanisclaws@users.noreply.github.com>
2024-06-12 18:55:58 -0400

    Update Scene1, EnemyAI, and backup scene

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,38 +3,42 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    //PLayer and NavMeshAgent agent:
     public NavMeshAgent agent;
     public Transform player;
 
+    //Layer Masks for ground and player
     public LayerMask whatIsGround, whatIsPlayer;
 
-    //Health Variables
-     public float health;
+    //Health Variables (Health of enemy):
+     public float health = 10f;
 
-    //Patroling
+    //Patroling Variables (Walkpoint, walkpoint range, and if walkpoint is set):
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
 
-    //Attacking
+    //Attacking player variables (Damage, time between attacks, and if enemy has already attacked):
     public float timeBetweenAttacks;
     bool alreadyAttacked;
     public GameObject projectile;
 
-    //States
+    //States of enemy (Patrol, Chase, Attack) and sight and attack range of enemy:
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
 
+    // Start is called before the first frame update (Find player and agent): 
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (Check for sight and attack range of player):
     void Update()
     {
-        //Check for sight of player and attack range
+    //Check for sight of player and attack range of player (If player is in sight range and not in attack range, patrol, if player is in sight range and
+    //in attack range, attack player, if player is not in sight range, patrol):
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
@@ -52,14 +56,14 @@ public class EnemyAI : MonoBehaviour
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        //Walkpoint reached
+        //Walkpoint reached if distance is less than 1 meter (Set walkpoint to false):
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
     }
 
     private void SearchWalkPoint()
     {
-        //Calculate random point in range
+        //Calculate random point in range of enemy:
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
@@ -84,12 +88,11 @@ public class EnemyAI : MonoBehaviour
         if (!alreadyAttacked)
         {
             //Attack code here (ANY ATTACK I WANT SWINGING SWORD, SHOOTING, ENERGY BLAST):
-
-            //Projectile:
              Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            //Force of projectile
+            
+            //Applying Force of projectile to move forward (32f is the speed of projectile):
              rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            //Upward force to make projectile go up
+            //Upward force to make projectile go up (8f is the speed of projectile going up):
              rb.AddForce(transform.up * 8f, ForceMode.Impulse);
             //player.GetComponent<Health>().TakeDamage(attackDamage);
              alreadyAttacked = true;
@@ -110,12 +113,13 @@ public class EnemyAI : MonoBehaviour
         if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
 
+    //Destroy enemy:
     private void DestroyEnemy()
     {
       Destroy(gameObject);
     }
 
-    //Visualize enemy sight and attack range
+    //Visualize enemy sight and attack range in editor:
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
